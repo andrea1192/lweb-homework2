7dd163bcf41c6d2c2c1fed27b769e0281e67f9a7
andrea1192 <andrea1192@users.noreply.github.com>
2024-05-21 13:55:07 +0200

    Aggiunto connection.php
    Aggiunto file richiesto con credenziali per test (fixes #1)

---
+++ html/connection.php
@@ -0,0 +1,10 @@
+<?php
+	$settings = [
+		'db_host' => 'db',
+		'db_user' => 'root',
+		'db_pass' => 'mariadb_root',
+		'db_name' => '1496769_lweb',
+		'app_user' => 'user',
+		'app_pass' => '1234'
+	];
+?>
